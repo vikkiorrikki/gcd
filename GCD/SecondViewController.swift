0f34a2a2bb80e06e258f57c85f3f4ffab888117f
Victoria Saklakova <viksi.saklakova@gmail.com>
2020-07-12 21:47:49 +0300

    issue with displaying high resolution image

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            
+            imageView.image = newValue
+            imageView.sizeToFit() // resize the current view
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://www.itl.cat/pngfile/big/253-2531591_roger-federer-high-quality-wallpapers-melhor-jogador-de.jpg")
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
     }
 }
+
